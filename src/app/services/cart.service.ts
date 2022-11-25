6c44074a1e96634f83235021689dab040bec0476
Rodolpho <redsmedeiros@gmail.com>
2022-11-25 16:48:16 -0300

    metodos do carrinho

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,6 +1,9 @@
+import { CartItem } from './../shared/models/CartItem';
 import { Cart } from './../shared/models/Cart';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { Food } from '../shared/models/Food';
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,4 +15,40 @@ export class CartService {
   private cartSubject: BehaviorSubject<Cart> = new BehaviorSubject(this.cart)
 
   constructor() { }
+
+  addToCart(food: Food): void{
+
+    let carItem = this.cart.items.find( item => item.food.id === food.id)
+
+    if(carItem){
+      return
+    }
+
+    this.cart.items.push(new CartItem(food))
+  }
+
+  revomeFromCart(foodId: string): void{
+
+    this.cart.items = this.cart.items.filter( item => item.food.id != foodId)
+
+  }
+
+  changeQuantity(foodId: string, quantity: number): void{
+
+    let cartItem = this.cart.items.find( item => item.food.id === foodId)
+
+    if(!cartItem){
+      return
+    }
+
+    cartItem.quantity = quantity
+
+    cartItem.price = quantity * cartItem.food.price
+
+  }
+
+  clearCart(){
+
+    this.cart = new Cart()
+  }
 }
